9f6f74eefb8c851eb5a3bd25f0197ddb3457567f
Tom Beynon <tombeynon@gmail.com>
2014-07-31 20:56:36 +0100

    Provide page controller as a mixin
    Needs a bit of thought

---
+++ lib/railgun_content/resources_controller/page.rb
@@ -0,0 +1,39 @@
+require 'railgun/resources_controller/positionable'
+
+module RailgunContent
+  module Page
+
+    def self.included(base)
+      base.instance_eval do
+        include Railgun::Positionable
+        
+    		option :icon, "file"
+
+        actions :all, :except => [:show]
+
+        attributes :meta_title, :meta_description, :meta_keywords, :viewable => false
+        attributes :ancestry, :position, :viewable => false, :editable => false
+
+        attributes :parent_id, :type => "nested_set", :viewable => false
+      end
+    end
+
+    def create
+      create! do |success, failure|
+        success.html{ redirect_to [:edit, resource] }
+      end
+    end
+
+    def update
+      update! do |success, failure|
+        success.html{ redirect_to [:edit, resource] }
+      end
+    end
+
+    protected
+
+    def collection
+      @pages ||= end_of_railgun_chain
+    end
+  end
+end
